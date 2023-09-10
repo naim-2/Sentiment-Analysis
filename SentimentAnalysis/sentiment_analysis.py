adf08eb20808395b9e92a414915566c22343a9f5
naim-2 <121217709+naim-2@users.noreply.github.com>
2023-09-10 18:27:05 +0300

    Updated the sentiment_analysis.py

--- SentimentAnalysis/sentiment_analysis.py
+++ SentimentAnalysis/sentiment_analysis.py
@@ -6,8 +6,8 @@ import requests
 
 def sentiment_analyzer(text_to_analyse):
     """Function to call sentiment analyzer from the Watson URL"""
-    url = """https://sn-watson-sentiment-bert.labs.skills.network/v1/
-    watson.runtime.nlp.v1/NlpService/SentimentPredict"""
+    url = "https://sn-watson-sentiment-bert.labs.skills.network/v\
+    1/watson.runtime.nlp.v1/NlpService/SentimentPredict"
     myobj = { "raw_document": { "text": text_to_analyse } }
     header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
     response = requests.post(url, json = myobj, headers=header, timeout=2.5)
